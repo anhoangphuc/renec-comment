5d9d7fd22d572a27a684b58d97643323a4f87082
hoangphuc <hoangphucnb97@gmail.com>
2024-05-01 20:48:00 +0700

    Add more tests

--- tests/dapp-starter.ts
+++ tests/dapp-starter.ts
@@ -29,10 +29,9 @@ describe("dapp-starter", () => {
 
   function getUserComment(user: PublicKey, count: number) {
     const [userComment] = PublicKey.findProgramAddressSync(
-        [Buffer.from("comment"), user.toBuffer()],
+        [Buffer.from("comment"), user.toBuffer(), Buffer.from([count])],
         program.programId,
     );
-
     return userComment;
   }
 
@@ -46,6 +45,76 @@ describe("dapp-starter", () => {
           systemProgram: anchor.web3.SystemProgram.programId,
         },
         signers: [user1],
+      });
+      console.log("Your transaction signature", tx);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+
+    // Get the new counter value
+    const userStateAccount = await program.account.userState.fetch(userState);
+    assert.equal(userStateAccount.commentCount, 0, "expect counter value to be 0");
+  });
+
+  it("User 1 add comment 1", async () => {
+    const userState = getUserState(user1.publicKey);
+    const userComment1 = getUserComment(user1.publicKey, 0);
+    try {
+      const tx = await program.rpc.addComment("Comment 1", {
+        accounts: {
+          userState,
+          signer: user1.publicKey,
+          userComment: userComment1,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [user1],
+      })
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+
+    const userComment1Account = await program.account.userComment.fetch(userComment1);
+    assert.equal(userComment1Account.comment, "Comment 1");
+    const userStateAccount = await program.account.userState.fetch(userState);
+    assert.equal(userStateAccount.commentCount, 1);
+  })
+
+  it("User 1 add comment 2", async () => {
+    const userState = getUserState(user1.publicKey);
+    const userComment2 = getUserComment(user1.publicKey, 1);
+    try {
+      const tx = await program.rpc.addComment("Comment 2", {
+        accounts: {
+          userState,
+          signer: user1.publicKey,
+          userComment: userComment2,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [user1],
+      })
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+
+    const userComment2Account = await program.account.userComment.fetch(userComment2);
+    assert.equal(userComment2Account.comment, "Comment 2");
+    const userStateAccount = await program.account.userState.fetch(userState);
+    assert.equal(userStateAccount.commentCount, 2);
+  })
+
+  it("User 2 register success", async () => {
+    const userState = getUserState(user2.publicKey);
+    try {
+      const tx = await program.rpc.userRegister({
+        accounts: {
+          userState,
+          signer: user2.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [user2],
       });
       console.log("Your transaction signature", tx);
     } catch (error) {
